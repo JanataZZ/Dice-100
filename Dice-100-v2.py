963f7a105e01e3c93569650d7bedcc95c9c542b4
Zhanat A <zhanat@assankulova.com>
2023-01-09 16:55:17 -0600

    miner update

--- Dice-100-v2.py
+++ Dice-100-v2.py
@@ -4,9 +4,10 @@ result = []
 for i in range (1,1001):
     sum = 0
     while sum <= 100:
-        sum += random.randint(1,6)
+        sum += random.randint(1,6) 
        
     result.append(sum)
+
 print(type(result))
 print(type(result[0]))
 print(result.count(101), result.count(102), result.count(103))
